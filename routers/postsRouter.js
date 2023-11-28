9292548a34e3feebcbd48c1db0072aafff10d3df
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-11-28 17:33:07 +0100

    modifica auth per accedere con vue

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -4,13 +4,13 @@ const postsController = require('../controllers/postsController');
 const authHandler = require('../middlewares/authHandler')
 
 
-router.post('/post', postsController.create)
+router.post('/post', postsController.create)//ho tolto autHeandler dalla rotta per accede con vue senza login
 router.get('/post', postsController.showAll)
 //filtrare per:
 // Post pubblicati.
 // Post che contengono una determinata parola nel titolo o nel contenuto.
 router.get('/post/:slug', postsController.show)
-router.put('/post/:slug', authHandler,  postsController.update)
+router.put('/post/:slug', postsController.update)
 router.delete('/post/:slug', authHandler, postsController.destroy)
 
 
